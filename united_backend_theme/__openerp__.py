382b8cfe6d2d51fcafed37a3758a9c0debbd92de
root <root@dock1.openworx.nl>
2016-07-27 19:13:08 +0200

    Screenshots added

--- united_backend_theme/__openerp__.py
+++ united_backend_theme/__openerp__.py
@@ -1,13 +1,16 @@
 {
     'name' : 'United based backend theme for Odoo 9c',
-    'version' : '0.1',
+    'version' : '0.2',
     'author' : 'Openworx',
     'category' : 'Website',
     'summary': 'Give Odoo 9 community version a cleaner backend theme',
     'website' : 'http://www.openworx.nl',
     'description': """
-Give Odoo 9 community version a cleaner theme. Based on Bootstrap Unity template.
+Give Odoo 9 community version a cleaner theme, based on Bootstrap Unity template. The theme has also some css fixes for Microsoft Explorer 11.
     """,
+    'images':[
+        'static/description/sales.png'
+    ],
     'depends' : ['base'],
     'data':[
         'views.xml',
