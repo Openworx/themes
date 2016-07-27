75893b3006d579ce242ded43e1fbbe305d75767d
root <root@dock1.openworx.nl>
2016-07-27 11:53:49 +0200

    first commit

---
+++ united_backend_theme/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'United based backend theme for Odoo 9c',
+    'version' : '0.1',
+    'author' : 'Openworx',
+    'category' : 'Website',
+    'summary': 'Give Odoo 9 community version a cleaner backend theme',
+    'website' : 'http://www.openworx.nl',
+    'description': """
+Give Odoo 9 community version a cleaner theme. Based on Bootstrap Unity template.
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
